946508e73cc9aeb51dd96658f6f926c0d8201f87
Anil <Anil@TYSS-MF-025>
2016-12-19 13:22:37 +0530

    official commit1

--- BuildshopAutomation/src/test/java/pages/LoginPage.java
+++ BuildshopAutomation/src/test/java/pages/LoginPage.java
@@ -1,10 +1,31 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	WebDriver driver;
-	@FindBy()
-
+	public LoginPage(WebDriver driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(xpath="(//input[@class='txt-sign-in'])[2]")
+    public WebElement un;
+    
+    @FindBy(xpath="(//input[@class='txt-sign-in'])[4]")
+    public WebElement pw;
+    
+    @FindBy(xpath="//input[@id='ctl00_ContentPlaceHolder1_Login101_Submit2']")
+    public WebElement signinButton;
+    
+    public void SignInMethod()
+    {
+        un.sendKeys("Bill Thomas");
+        pw.sendKeys("Bill1234");
+        signinButton.click();
+    }
+	
 }
