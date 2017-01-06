cd5e8080a52b1ad78ad92c23858283337b2efa38
SBRakesh <rakeshsb313@gmail.com>
2017-01-06 16:19:09 +0530

    Add files via upload

---
+++ BuildshopAutomation/src/test/java/pages/HomeDashboardPage.java
@@ -0,0 +1,45 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomeDashboardPage 
+{
+	//Plan button
+	@FindBy(xpath="//a[text()='Plan']")
+	private WebElement planButton;
+	
+	//Drawing button
+	@FindBy(xpath="//a[text()='Drawings']")
+	private WebElement drawingbutton;
+		
+	//click on procure module
+	@FindBy(xpath="//a[text()='Procure']")
+	private WebElement procure;
+
+	//constructor
+	public HomeDashboardPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver,this);
+	}
+	
+	//click on plan button
+	public void clickPlanButton()
+	{
+		planButton.click();
+	}
+	
+	//click on drawing button
+	public void clickDrawingButton()
+	{
+		drawingbutton.click();
+	}
+	
+	//to click on procure module
+	public void clickOnProcure()
+	{
+		procure.click();
+	}
+}
